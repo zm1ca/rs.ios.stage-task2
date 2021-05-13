81e1cb30503ed7c089ba5e5ca0b66c891519e521
zm1ca <zmicier.fied@yandex.ru>
2021-05-13 20:19:14 +0300

    feat: add implementation for exercise 1

--- Objc_Task2/ios.stage-task/Exercises/Exercise-1/ReverseBinary.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-1/ReverseBinary.m
@@ -1,5 +1,12 @@
 #import "ReverseBinary.h"
 
 UInt8 ReverseInteger(UInt8 n) {
-    return 0;
+    UInt8 reversed = 0;
+    
+    for (int i = 0; i < 8; ++i) {
+        reversed |= (n & 1) << (7 - i);
+        n >>= 1;
+    }
+    
+    return reversed;
 }
